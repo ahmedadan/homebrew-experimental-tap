a15a4902938dc2af2288ed2c802af8112515db85
Ahmed Adan <ahmed.adan@gmail.com>
2025-12-19 14:52:02 -0500

    fix(ci): ran brew style . --fix

--- Casks/bluefin-cli.rb
+++ Casks/bluefin-cli.rb
@@ -1,29 +1,23 @@
 cask "bluefin-cli" do
-  version "0.0.1"
   arch arm: "_arm64", intel: "_amd64"
+
+  version "0.0.1"
+
   os_map = { linux: "linux", macos: "darwin" }
 
   on_linux do
-    on_intel do
-      sha256 "edd352b9de6bd05d5248b63daecb6d0bc6252eb0ad500fdb3d7d790083a429d5"
-    end
-    on_arm do
-      sha256 "8ba6aa87ef00a3f0d05051d143e625227cbb7dec720804364e8cc8f5402a3449"
-    end
+    sha256 arm:   "8ba6aa87ef00a3f0d05051d143e625227cbb7dec720804364e8cc8f5402a3449",
+           intel: "edd352b9de6bd05d5248b63daecb6d0bc6252eb0ad500fdb3d7d790083a429d5"
   end
 
   on_macos do
-    on_intel do 
-      sha256 "fcfd4bcf9723ad03b9fe1591da286242cf7651ea7b9f7c20f5ec087201cf3815"
-    end
-    on_arm do
-      sha256 "c930feab1acd0fb6bfc632356acb54f56428ee980455f2e358e79f9880a8dca7"
-    end
+    sha256 arm:   "c930feab1acd0fb6bfc632356acb54f56428ee980455f2e358e79f9880a8dca7",
+           intel: "fcfd4bcf9723ad03b9fe1591da286242cf7651ea7b9f7c20f5ec087201cf3815"
   end
 
   url "https://github.com/hanthor/bluefin-cli/releases/download/v#{version}/bluefin-cli_#{version}_#{os_map[Homebrew::SimulateSystem.current_os]}#{arch}.tar.gz"
   name "Bluefin CLI"
-  desc "Bluefin CLI tool"
+  desc "Bluefin's CLI tool"
   homepage "https://github.com/hanthor/bluefin-cli"
 
   livecheck do
