a773ae0fe0a70296365493e94ad9032c6db976e5
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2025-12-20 12:51:23 +0000

    bluefin-cli 0.0.2

--- Casks/bluefin-cli.rb
+++ Casks/bluefin-cli.rb
@@ -1,23 +1,23 @@
 cask "bluefin-cli" do
-  version "0.0.1"
+  version "0.0.2"
 
   on_arm do
     on_linux do
-      sha256 "8ba6aa87ef00a3f0d05051d143e625227cbb7dec720804364e8cc8f5402a3449"
+      sha256 "5294bbb262aa80fd20d17e00295bf1a2c770f32b1cf1d1ee82b80ba448716e94"
       url "https://github.com/hanthor/bluefin-cli/releases/download/v#{version}/bluefin-cli_#{version}_linux_arm64.tar.gz"
     end
     on_macos do
-      sha256 "c930feab1acd0fb6bfc632356acb54f56428ee980455f2e358e79f9880a8dca7"
+      sha256 "cbcab08545ac221acea714bfbfff0380762c08f6884d1c3eaed40e309ba3ea7f"
       url "https://github.com/hanthor/bluefin-cli/releases/download/v#{version}/bluefin-cli_#{version}_darwin_arm64.tar.gz"
     end
   end
   on_intel do
     on_linux do
-      sha256 "edd352b9de6bd05d5248b63daecb6d0bc6252eb0ad500fdb3d7d790083a429d5"
+      sha256 "e6fec4f0366d5526f0bb7ab551b17243414deb19bbbecfdc7bd06c91f9cdc061"
       url "https://github.com/hanthor/bluefin-cli/releases/download/v#{version}/bluefin-cli_#{version}_linux_amd64.tar.gz"
     end
     on_macos do
-      sha256 "fcfd4bcf9723ad03b9fe1591da286242cf7651ea7b9f7c20f5ec087201cf3815"
+      sha256 "cb6f2f7539dac967b036328eb7feefb0ba931af3b31d643c0491b2a11c5977a9"
       url "https://github.com/hanthor/bluefin-cli/releases/download/v#{version}/bluefin-cli_#{version}_darwin_amd64.tar.gz"
     end
   end
